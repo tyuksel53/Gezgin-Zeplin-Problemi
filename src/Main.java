3535ba873e86d6f97f52e76a9e106abf582a015c
Taha Yüksel <taha.yuksel053@gmail.com>
2018-03-02 14:34:04 +0300

    tasarım tamamlandı.
    ...

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import MyJFrame.EkraniOlustur;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String argms[])
     {
-        System.out.println("Hello world");
+        new EkraniOlustur();
     }
 }
